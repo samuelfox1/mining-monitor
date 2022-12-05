25e67cf71d7ac6db7de730517a5a0f4fa2f9221e
sjfox <samueljasonfox@gmail.com>
2022-12-04 18:57:57 -0800

    tidy up

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import dotenv from "dotenv";
+import { config } from "dotenv";
 import { Vonage } from "@vonage/server-sdk";
 
-dotenv.config();
+config();
+
+const {
+  env: { API_KEY, API_SECRET, VIRTUAL_NUMBER, TO_NUMBER },
+} = process;
 
 const vonage = new Vonage(
   {
-    apiKey: process.env.VONAGE_API_KEY,
-    apiSecret: process.env.VONAGE_API_SECRET,
-    // applicationId: process.env.VONAGE_APPLICATION_ID,
-    // privateKey: process.env.VONAGE_PRIVATE_KEY_PATH,
+    apiKey: API_KEY,
+    apiSecret: API_SECRET,
   },
   { debug: true }
 );
 
 const params = {
-  from: process.env.VONAGE_VIRTUAL_NUMBER,
-  to: process.env.VONAGE_TO_NUMBER,
+  from: VIRTUAL_NUMBER,
+  to: TO_NUMBER,
   text: "Hello from node!",
 };
 
